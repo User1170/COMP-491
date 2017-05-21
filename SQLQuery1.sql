a2add80a4678293217f4160854ce9d67f2f38e7c
Hakob Zakaryan <Hakob.zakaryan.999@my.csun.edu>
2017-05-20 20:32:52 -0700

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO dbo.Project  
+(  
+    ProjectName,  
+    StartDate,  
+     EndDate  
+)  
+VALUES( N'Lions',CAST('02/01/2017' as datetime),CAST('04/05/2017' AS datetime) ),(  N'OUP',CAST('08/09/2016' AS datetime),CAST('12/03/2017' AS datetime) ),  
+(   N'VMesh',CAST('12/04/2016' as date),CAST('04/01/2017' as date) )
---
+++ SQLQuery3.sql
@@ -0,0 +1,7 @@
+﻿insert into Employee  
+(Designation,EmployeeName,ProjectId,Skills)  
+values('Developer','Raj Kumar',2,'C#,Asp.Net,MVC'),  
+('Mobile Developer','Ankur Verma',3,'Java, Android Studio, Xamarin'),  
+('Developer','Dheeraj Sharma',1,'C#,Asp.Net,MVC,AngularJS'),  
+('Developer','Dhramveer',2,'C#,Asp.Net,MVC,AngularJS,Node.js'),  
+('Mobile Developer','Shivam Kumar',1,'Java, Android Studio, Xamarin')
